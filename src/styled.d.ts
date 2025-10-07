26f62467179de2161bec1522d76092794865fb39
aram0127 <a67891@naver.com>
2025-10-07 23:55:45 +0900

    Delete src directory

--- src/styled.d.ts
+++
@@ -1,9 +0,0 @@
-import 'styled-components';
-import { lightTheme } from './styles/theme';
-
-// lightTheme 객체의 타입을 추론
-type Theme = typeof lightTheme;
-
-declare module 'styled-components' {
-  export interface DefaultTheme extends Theme {}
-}
