f8dde9d0a7b1939f9574b5c36cfd0f40d6f7ca0a
junhyuk Hyun <sd7563@gmail.com>
2025-10-15 17:39:00 +0900

    Post 기본구조

---
+++ campustrip-back/src/main/java/com/example/app/controller/PostController.java
@@ -0,0 +1,34 @@
+package com.example.app.controller;
+
+import com.example.app.domain.Post;
+import com.example.app.service.PostService;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+    private final PostService postService;
+
+    @Autowired
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    // GET: ID로 게시물 조회
+    @GetMapping("/{post_id}")
+    public Post getPostById(@PathVariable Integer post_id) {
+        return postService.getPostById(post_id);
+    }
+
+    // POST: 새 게시물 생성
+    @PostMapping
+    public Post createPost(@RequestBody Post post) {
+        postService.savePost(post);
+        return post;
+    }
+
+    // PUT: 게시물 정보 수정
+
+    // DELETE: 게시물 삭제
+}
