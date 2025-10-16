ff1af6af156b7f4f34ef0fc576e43005932aca3c
junhyuk Hyun <sd7563@gmail.com>
2025-10-16 21:10:01 +0900

    지역ID 목록으로 게시글 조회 사용자가 작성한 게시글 조회 지역ID와 게시글 연관관계 추가

--- campustrip-back/src/main/java/com/example/app/repository/PostRepository.java
+++ campustrip-back/src/main/java/com/example/app/repository/PostRepository.java
@@ -1,9 +1,11 @@
 package com.example.app.repository;
 
 import com.example.app.domain.Post;
+import com.example.app.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post,Integer>
 {
@@ -12,6 +14,15 @@ public interface PostRepository extends JpaRepository<Post,Integer>
 
     //findByUser() List
     //find(term, regionList, page, size): List
+
+    // membershipId로 Post 찾기
+
+    List<Post> findAllByUser(User user);
+
+    // regionId로 Post 찾기
+    @Query("SELECT DISTINCT p FROM Post p JOIN FETCH p.regions r WHERE r.regionId IN :regionIds")
+    List<Post> findAllByRegionIds(@Param("regionIds") List<Integer> regionIds);
+
 
     // 동적 쿼리 생성
 //    @Query(value= "select m from Member m where m.memberId = :id and m.email = :email" )
--- campustrip-back/src/main/java/com/example/app/service/PostService.java
+++ campustrip-back/src/main/java/com/example/app/service/PostService.java
@@ -1,37 +1,51 @@
 package com.example.app.service;
 
+import com.example.app.domain.User;
+import com.example.app.repository.UserRepository;
 import org.springframework.stereotype.Service;  // @Service 어노테이션
 import org.springframework.beans.factory.annotation.Autowired;  // 의존성 주입용 (선택적)
 import com.example.app.domain.Post;
 import com.example.app.repository.PostRepository;
-
 import java.util.List;
 import java.util.NoSuchElementException;
 
 @Service
 public class PostService {
     private final PostRepository postRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    public PostService(PostRepository postRepository) {
+    public PostService(PostRepository postRepository,
+                       UserRepository userRepository) {
         this.postRepository = postRepository;
+        this.userRepository = userRepository;
     }
 
     public List<Post> getAllPosts() {
         return postRepository.findAll();
     }
 
-    public Post getPostById(Integer post_id) {
-        return postRepository.findById(post_id)
-                .orElseThrow(() -> new NoSuchElementException("Post not found with id: " + post_id));
+    public Post getPostById(Integer postId) {
+        return postRepository.findById(postId)
+                .orElseThrow(() -> new NoSuchElementException("Post not found with id: " + postId));
+    }
+
+    public List<Post> getPostsByMembershipId(Integer membershipId) {
+        User user = userRepository.findById(membershipId)
+                .orElseThrow(() -> new NoSuchElementException("User not found with id: " + membershipId));
+        return postRepository.findAllByUser(user);
+    }
+
+    public List<Post> getPostsByRegionIds(List<Integer> regionIds) {
+        return postRepository.findAllByRegionIds(regionIds);
     }
 
     public void savePost(Post post) {
         postRepository.save(post);
     }
 
-    public void deletePost(Integer post_id) {
-        postRepository.deleteById(post_id);
+    public void deletePost(Integer postId) {
+        postRepository.deleteById(postId);
     }
 
 }
