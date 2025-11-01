c7d707de482d0362748ab9dd6ff46e5369f9a3b4
junhyuk Hyun <sd7563@gmail.com>
2025-11-01 16:19:14 +0900

    플래너 생성 수정 조회 삭제 추가

---
+++ campustrip-back/src/main/java/com/example/app/dto/CreatePlanner.java
@@ -0,0 +1,17 @@
+package com.example.app.dto;
+
+import com.example.app.domain.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreatePlanner {
+    private User user; // 플래너 작성자의 membership_id
+    private java.time.LocalDate startDate;
+    private java.time.LocalDate endDate;
+}
