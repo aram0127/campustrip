f5c6d67defe782817b76c35d1af22fbbdc7f0720
junhyuk Hyun <sd7563@gmail.com>
2025-11-01 14:35:18 +0900

    후기게시글도 추가예정

--- campustrip-back/src/main/java/com/example/app/domain/ReviewPost.java
+++ campustrip-back/src/main/java/com/example/app/domain/ReviewPost.java
@@ -1,8 +1,8 @@
-package com.example.app.domain;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-
-@Entity
-public class ReviewPost {
-}
+//package com.example.app.domain;
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Table;
+//
+//@Entity
+//public class ReviewPost {
+//}
