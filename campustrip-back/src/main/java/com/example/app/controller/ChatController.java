7cbf456a3ea86aba000fb2177c490bd6b284f78a
junhyuk Hyun <sd7563@gmail.com>
2025-11-07 16:22:51 +0900

    monogoDB에 채팅내역 저장하도록 추가

--- campustrip-back/src/main/java/com/example/app/controller/ChatController.java
+++ campustrip-back/src/main/java/com/example/app/controller/ChatController.java
@@ -2,7 +2,8 @@ package com.example.app.controller;
 
 
 import com.example.app.dto.ChatDTO;
-import com.example.app.dto.ChatMessage;
+import com.example.app.dto.ChatMessageDTO;
+import com.example.app.service.ChatMessageService;
 import com.example.app.service.ChatService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -15,16 +16,18 @@ import java.util.List;
 @RequestMapping("/api/chats")
 public class ChatController {
     ChatService chatService;
-    private final KafkaTemplate<String, ChatMessage> kafkaTemplate;
+    private final KafkaTemplate<String, ChatMessageDTO> kafkaTemplate;
+    private final ChatMessageService chatMessageService;
 
     @Autowired
-    public ChatController(ChatService chatService, KafkaTemplate<String, ChatMessage> kafkaTemplate) {
+    public ChatController(ChatService chatService, ChatMessageService chatMessageService, KafkaTemplate<String, ChatMessageDTO> kafkaTemplate) {
         this.chatService = chatService;
         this.kafkaTemplate = kafkaTemplate;
+        this.chatMessageService = chatMessageService;
     }
 
     @MessageMapping("/chat/message")  // 클라이언트가 /pub/chat/message로 전송
-    public void sendMessage(ChatMessage message) {
+    public void sendMessage(ChatMessageDTO message) {
         System.out.println("메시지 수신: {}" + message);
 
         // ❌ 기존: SimpMessagingTemplate으로 직접 전송
@@ -35,7 +38,7 @@ public class ChatController {
         kafkaTemplate.send(topic, message);
 
         // MongoDB에 채팅 내역 저장 (선택사항)
-        // chatRepository.save(message);
+        chatMessageService.saveMessage(message);
     }
 
     @GetMapping("/chat/{id}")
