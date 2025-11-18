efedd9c7e6b99d3bb24d7762c0efa73285a6442e
junhyuk Hyun <sd7563@gmail.com>
2025-11-18 20:36:37 +0900

    후기 게시글 + 사진 클래스 기본 틀 구성

---
+++ campustrip-back/src/main/java/com/example/app/domain/ReviewAssetId.java
@@ -0,0 +1,19 @@
+package com.example.app.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class ReviewAssetId implements Serializable {
+    private Integer assetId;
+    private Integer reviewId;
+}
---
+++ campustrip-back/src/main/java/com/example/app/dto/CreateReview.java
@@ -0,0 +1,37 @@
+package com.example.app.dto;
+
+import com.example.app.domain.Post;
+import com.example.app.domain.Review;
+import com.example.app.domain.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateReview {
+    private Integer reviewId;
+    private User user;
+    private Post post;
+    private String title;
+    private String body;
+    private java.time.LocalDateTime createdAt = java.time.LocalDateTime.now();
+
+    // 첨부 이미지 파일 - aws에 업로드 후 url로 저장 예정
+
+    public Review toEntity() {
+        Review newReview = new Review();
+        newReview.setUser(this.user);
+        newReview.setPost(this.post);
+        newReview.setTitle(this.title);
+        newReview.setBody(this.body);
+        newReview.setCreatedAt(this.createdAt);
+        newReview.setUpdatedAt(this.createdAt);
+        return newReview;
+    }
+}
