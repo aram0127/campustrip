d8f5f05364500c56f0354422b26668cde97b46b9
Junyeong-Ha <hth45123@naver.com>
2025-11-21 16:43:06 +0900

    Review 추가

---
+++ campustrip-back/src/main/java/com/example/app/controller/ReviewController.java
@@ -0,0 +1,22 @@
+package com.example.app.controller;
+
+
+import com.example.app.domain.Review;
+import com.example.app.dto.ReviewDTO;
+import com.example.app.service.ReviewService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/reviews")
+public class ReviewController {
+    private final ReviewService reviewService;
+
+    public ReviewController(ReviewService reviewService) {
+        this.reviewService = reviewService;
+    }
+
+    @GetMapping("/{id}")
+    public ReviewDTO getReviewById(@PathVariable Integer id) {
+        return reviewService.getReviewById(id);
+    }
+}
--- campustrip-back/src/main/java/com/example/app/service/ReviewService.java
+++ campustrip-back/src/main/java/com/example/app/service/ReviewService.java
@@ -1,9 +1,14 @@
 package com.example.app.service;
 
+import com.example.app.domain.Review;
+import com.example.app.dto.ReviewDTO;
 import com.example.app.repository.ReviewAssetRepository;
 import com.example.app.repository.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ReviewService {
@@ -16,6 +21,17 @@ public class ReviewService {
         this.reviewAssetRepository = reviewAssetRepository;
     }
 
-
-    // 이미지받아서 ec2에 올리고 url을 reviewAsset에 저장하는 메서드들 작성 예정
+    public ReviewDTO getReviewById(Integer id) {
+        Review review = reviewRepository.findById(id).orElse(null);
+        if (review == null) {
+            return null; // 또는 예외 처리
+        }
+        ReviewDTO reviewDTO = new ReviewDTO(review);
+        List<String> assetList = new ArrayList<>();
+        reviewAssetRepository.findAllByReviewId(id).forEach(asset -> {
+            assetList.add(asset.getStorageUrl());
+        });
+        reviewDTO.setImageUrls(assetList);
+        return reviewDTO;
+    }
 }
