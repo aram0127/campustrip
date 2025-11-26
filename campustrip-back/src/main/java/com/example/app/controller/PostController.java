66987a16737ebd713769cf9148608bd178d51d67
junhyuk Hyun <sd7563@gmail.com>
2025-11-26 20:09:43 +0900

    post에 slice 로딩 api 기능 추가

--- campustrip-back/src/main/java/com/example/app/controller/PostController.java
+++ campustrip-back/src/main/java/com/example/app/controller/PostController.java
@@ -7,6 +7,8 @@ import com.example.app.service.ChatService;
 import com.example.app.service.PostService;
 import com.example.app.service.RegionService;
 import com.example.app.service.S3Service;
+import org.springframework.data.domain.*;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,6 +40,31 @@ public class PostController {
 
         // 엔티티 리스트를 DTO 리스트로 변환
         return posts.stream().map(post -> postService.convertPostToDTO(post, chatService, regionService)).collect(Collectors.toList());
+    }
+//      page 대신 slice로 바꾸기 - pagable 쓰는 코드 참고용임
+//    // GET: 전체 게시물 조회 (페이징 처리)
+//    //Parameter로 페이지번호(pageNo), 정렬 기준(criteria)을 받는다.
+//    //default 값은 페이지 번호 0, 정렬 기준 createdAt(작성일자)이다.
+//    // /paged?pageNo=1&size=10&sort=createdAt
+//    @GetMapping("/paged")
+//    public Page<PostDTO> getAllPostsPaged(
+//            @RequestParam(required = false, defaultValue = "0", value="page") int pageNo,
+//            @RequestParam(required = false, defaultValue = "5", value = "size") int size,
+//            @RequestParam(required = false, defaultValue = "createdAt", value="sort") String sort) {
+//
+//        Pageable pageable = PageRequest.of(pageNo, size, Sort.by(Sort.Direction.DESC, sort));
+//        Page<Post> postPage = postService.getAllPostsPaged(pageable);
+//        // 엔티티 페이지를 DTO 페이지로 변환
+//        return postPage.map(post -> postService.convertPostToDTO(post, chatService, regionService));
+//    }
+    @GetMapping("/infinite")
+    public Slice<PostDTO> getAllPostsInfinite(
+            @PageableDefault(size = 3, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
+        Slice<Post> postSlice = postService.getAllPostsSlice(pageable);
+        System.out.println("요청된 페이지 크기(Size): " + pageable.getPageSize());
+        System.out.println("요청된 페이지 번호(Page): " + pageable.getPageNumber());
+        // 엔티티 슬라이스를 DTO 슬라이스로 변환
+        return postSlice.map(post -> postService.convertPostToDTO(post, chatService, regionService));
     }
 
     // GET: ID로 게시물 조회 (기존 로직을 DTO 변환 메서드로 분리)
--- campustrip-back/src/main/java/com/example/app/repository/PostRepository.java
+++ campustrip-back/src/main/java/com/example/app/repository/PostRepository.java
@@ -2,6 +2,9 @@ package com.example.app.repository;
 
 import com.example.app.domain.Post;
 import com.example.app.domain.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
