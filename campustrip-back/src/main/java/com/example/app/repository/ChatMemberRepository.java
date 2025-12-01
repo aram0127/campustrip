6b7c6a00ea0a12ea89d38106629cff9e7fbf2778
Junyeong-Ha <hth45123@naver.com>
2025-12-01 18:23:11 +0900

    채팅방 목록에 프로필 사진 전송

--- campustrip-back/src/main/java/com/example/app/repository/ChatMemberRepository.java
+++ campustrip-back/src/main/java/com/example/app/repository/ChatMemberRepository.java
@@ -4,6 +4,7 @@ import com.example.app.domain.Chat;
 import com.example.app.domain.ChatMember;
 import com.example.app.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,5 +15,8 @@ public interface ChatMemberRepository  extends JpaRepository<ChatMember, ChatMem
     List<ChatMember> findAllByChat(Chat chat);
     ChatMember findByChatAndUser(Chat chat, User user);
     void deleteByChatAndUser(Chat chat, User user);
+    // chatId 리스트로 ChatMember 찾기 (role이 true인 멤버만)
+    @Query("SELECT cm FROM ChatMember cm LEFT JOIN FETCH cm.user WHERE cm.chat.id IN :chatIds AND cm.role = true ORDER BY cm.chat.id ASC")
     List<ChatMember> findAllByChatIdIn(List<Integer> chatIds);
+
 }
